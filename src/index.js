ce66fa4228a71144588dc20815c00f0ade84503b
Ahnaf Ahmad <ahrmada95@gmail.com>
2022-07-08 09:41:19 -0400

    finsihed everything

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 //urls
 const imgsUrl = 'http://localhost:3000/images/';
 const commentsUrl = 'http://localhost:3000/comments/';
+const randoDogUrl = 'https://dog.ceo/api/breeds/image/random';
 
 //get center card title
 const postTitle = document.querySelector('#card-title');
@@ -16,7 +17,6 @@ const commentList = document.querySelector('#comments-list');
 const newCommentForm = document.querySelector('#comment-form');
 //global variables
 let imgId = 0;
-
 
 //images fetch request function
 const fetchPostImg = async () => {
@@ -56,19 +56,35 @@ const renderCard = async () => {
     imgId = card['id'];
 }
 
-//render the comments
+//render the comments THIS ALSO HANDLES DELETING COMMENTS ON PRESS
 const renderComments = async () => {
     //first clean out comments
     commentList.innerHTML = '';
     //fetch information
     const postComments = await fetchPostComments();
-    //iterate through and append
-    for(comment of postComments) {
+    //iterate through and append, have to use forEach so the iterator doesn't get stuck at the final entry (because fuck me, I guess)
+    postComments.forEach(comment => {
         let tempElem = document.createElement('li');
         tempElem.classList.add('comments')
         tempElem.textContent = comment['content'];
         commentList.append(tempElem);
-    }
+
+        //DELETION functionality: add event listener for comment press 
+        tempElem.addEventListener('click', () => {
+            if(confirm('Are you sure you want to delete this comment?')){
+                //delete request to get it out of the DB
+                fetch(`${commentsUrl}/${comment['id']}`,{
+                    method: 'DELETE',
+                }).then ((response) => {
+                    //delete comment visually (this way, renderComments() is not called recursively
+                    tempElem.remove();
+                })
+            } else {
+                //user has indicated they do not want to delete comment
+                alert('Comment removal cancelled.');
+            }   
+        })
+    })
 }
 
 //functionality for like button
@@ -80,7 +96,7 @@ likeBtn.addEventListener('click', async (event) => {
     //get the number of likes currently and increment by 1
     const currLikes = card['likes'] + 1;
     //http request
-    let updateLikes = await fetch(`${imgsUrl}/1`, {
+    let updateLikes = await fetch(`${imgsUrl}/${imgId}`, {
         //patch new likes
         method: 'PATCH',
         body: JSON.stringify({
@@ -101,38 +117,65 @@ newCommentForm.addEventListener('submit', async (event) => {
     event.preventDefault();
     //get user input
     let userInput = newCommentForm['comment'].value;
-    // //check for valid input and then post
-    // if(userInput != ''){
-    //     let newComment = document.createElement('li');
-    //     newComment.classList.add('comments');
-    //     newComment.textContent = userInput;
-    //     commentList.append(newComment);
-    // } else {
-    //     alert('invalid entry')
-    // }
-    //check if blank string
-    
-    let commentObj = {
-        'imageId': imgId,
-        'content': userInput,
-    }
-    let newEntryDb = JSON.stringify(commentObj);
-
-    let addNew = await fetch('http://localhost:3000/comments/', {
-        method: "POST",
-        header: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-        },
-        body: JSON.stringify({
-            "id": 4,
-            "imageId": 1,
-            "content": "fuck this shit what the fuck"
+    //make sure user input isn't empty
+    if(userInput != ''){
+        //post that shit into the db
+        let addNew = await fetch('http://localhost:3000/comments/', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                imageId: imgId,
+                content: userInput
         }),
     })
+    } else {
+        alert('Invalid input, please type something!')
+    }
     //clear entry
     newCommentForm['comment'].value = '';
+    //re-render comments
+    renderComments();
 })
+
+//toggle viewing the post image
+postTitle.addEventListener('click', () => {
+    //check to see if the img is already hidden
+    if(!postImg.classList.contains('hidden')){
+        //image is not hidden, so make it hidden
+        postImg.classList.add('hidden');
+    } else {
+        //hide the image
+        postImg.classList.remove('hidden');
+    }
+})
+
+//change img to rando doggo 
+postImg.addEventListener('click', async() => {
+    //generate random img url
+    const req = await fetch(randoDogUrl);
+    //convert to json
+    const res = await req.json();
+    //save url into var
+    const newImgUrl = res['message'];
+    console.log(newImgUrl);
+    //change src of img to new url
+    postImg.src = newImgUrl;
+
+    //return the patch
+    return fetch(`${imgsUrl}/${imgId}`, {
+        //patch new img url
+        method: 'PATCH',
+        body: JSON.stringify({
+            //edit url
+            'image': newImgUrl,
+        }),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+});
 
 renderCard();
 renderComments();
